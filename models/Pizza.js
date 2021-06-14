69706b5ddfdc56c9190114857717c3e956deaaf5
Your Name <you@example.com>
2021-06-14 18:12:20 -0400

    comment-routes

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,6 +1,8 @@
 const { Schema, model }= require('mongoose');
 
-const PizzaSchema= new Schema({
+
+const PizzaSchema= new Schema(
+    {
     pizzaName: {
         type: String
     },
@@ -15,7 +17,25 @@ const PizzaSchema= new Schema({
         type: String,
         default: 'Large'
     },
-    toppings: []
+    toppings: [],
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
+},
+{
+    toJSON: {
+        virtuals: true,
+    },
+    id: false
+}
+);
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
 });
 
 // create the Pizza model using the PizzaSchema
