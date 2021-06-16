8990fec08696244abf237c2564ed0cd06b3d463b
Your Name <you@example.com>
2021-06-16 10:17:55 -0400

    pizza-list

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,4 +1,5 @@
 const { Schema, model }= require('mongoose');
+const dateFormat= require('../utils/dateFormat');
 
 
 const PizzaSchema= new Schema(
@@ -11,7 +12,8 @@ const PizzaSchema= new Schema(
     },
     createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
     },
     size: {
         type: String,
@@ -28,6 +30,7 @@ const PizzaSchema= new Schema(
 {
     toJSON: {
         virtuals: true,
+        getters: true
     },
     id: false
 }
