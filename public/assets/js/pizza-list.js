a1d5aa406449d9bda52056fe761fc8fe408d82c1
Your Name <you@example.com>
2021-06-17 19:56:46 -0400

    ff

--- public/assets/js/pizza-list.js
+++ public/assets/js/pizza-list.js
@@ -1,5 +1,5 @@
 const $pizzaList = document.querySelector('#pizza-list');
-const fetch= require('node-fetch');
+// const fetch= require('node-fetch');
 
 const getPizzaList = () => {
   fetch('/api/pizzas')
