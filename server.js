4b87c16143ddb017e12087892a71010a440e6901
Your Name <you@example.com>
2021-06-17 20:13:07 -0400

    ff

--- server.js
+++ server.js
@@ -8,14 +8,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use(require('./routes'));
-
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
     useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
+
+app.use(require('./routes'));
+
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
